5466eb3c43df9359e53be79e2ce94d61b692ce93
Roshan Thapa <roshan@Roshans-MacBook-Air.local>
2023-05-25 13:43:53 +0545

    Team member photo added

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./Cards.css";
 
+
+
 function Cards(props) {
 
     const cardObjects=props.cardObjects;
-
   return (
     <div className="Cards-Wrapper flex-r-se-c">
         {
@@ -19,20 +20,25 @@ function Cards(props) {
 // This card is for team member only, components are on that basis so..
 function Card(props)
 {
+    const MemberImageExist= ()=>
+    {
+        try{return require("../Images/Team Members/"+cardObject[0]+".jpg");}
+        catch{return null}
+    }
     
     const cardObject=props.obj;
 
     return (
         <div className="Card-Cont flex-c-se-c">
-            <img className="Member-Photo" src={cardObject[0]} alt="Member Photo" />
-            <span className="Member-Name text">{cardObject[1]}</span>
-            <span className="Member-Depart text">{cardObject[2]}</span>
-            <span className="Member-Email text">{cardObject[3]}</span>
+           {/* use tryRequire if not sure team member photo exists or not */}
+           <img className="Member-Photo" src={MemberImageExist()?MemberImageExist():""} alt="Member Photo" />
+            <span className="Member-Name text">{cardObject[0]}</span>
+            <span className="Member-Depart text">{cardObject[1]}</span>
+            <span className="Member-Email text">{cardObject[2]}</span>
             <div className="Socials flex-row-evenly">
                 <span className="Facebook">F</span>
-                <a href={cardObject[4]} className="Linkedin">L</a>
+                <a href={cardObject[3]} className="Linkedin">L</a>
             </div>
-
         </div>
     )
 }
