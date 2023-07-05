0fa6ec381e056b567cb14afcdce452dd9806e7ee
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-07-05 21:28:18 +0545

    data seperated in object in js

---
+++ src/Data/MenuItems.js
@@ -0,0 +1,41 @@
+export const MenuItems = [
+    {
+        item : "Home",
+        url : "/Home",
+    },
+    {
+        item : "Teams",
+        url : "/Teams",
+    },
+    {
+        item : "Partnership",
+        url : "/Partnership",
+    },
+    {
+        item : "Our Projects",
+        url : "/OurProjects",
+    },
+    {
+        item : "Resources",
+        url : "/Resources",
+    },
+    {
+        item : "Contact Us",
+        url : "/ContactUs",
+    }
+]
+
+export const QuickMenuItems = [
+    {
+        item : "References",
+        url : "/References",
+    },
+    {
+        item : "Events",
+        url : "/Events",
+    },
+    {
+        item : "Contact Us",
+        url : "/ContactUs",
+    }
+]
---
+++ src/Data/SocialLinks.js
@@ -0,0 +1,14 @@
+export const _SocialLinks = [
+    {
+        media : "Facebook",
+        url : "https://www.facebook.com/teamshireto/",
+    },
+    {
+        media : "Linkedin",
+        url : "https://www.linkedin.com/company/team-shireto/",
+    },
+    {
+        media : "Youtube",
+        url : "https://www.youtube.com/@teamshireto1629/featured",
+    }
+]
