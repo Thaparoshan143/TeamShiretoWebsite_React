c7dd9e22da285a6a5a5636adc11234a8b7f7d835
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-07-06 08:56:55 +0545

    data reorganized and few update

--- src/Components/OurProjects.js
+++ src/Components/OurProjects.js
@@ -2,51 +2,16 @@ import "../uni.css";
 import "./OurProjects.css";
 import FrontLook from "../Images/DesignImage/FrontView.jpg";
 import TopLook from "../Images/DesignImage/AlmostTop.jpg";
-
-class Progress
-{
-	stageNum;
-	timeDuration;
-	mainWork;
-	
-	constructor(num,duration,workinfo)
-	{
-		this.stageNum=num;
-		this.timeDuration=duration;
-		this.mainWork=workinfo
-	}
-};
-
-const stage1=new Progress(1,"Nov - Dec","Preliminary Research");
-const stage2=new Progress(2,"Jan - Mar","Preliminary Vehicle Design, Cosf Estimation & Sponsorhip");
-const stage3=new Progress(3,"Apr - May","Final Design Approval & Material Procurement");
-const stage4=new Progress(4,"May - June","Event Registration & Quix");
-const stage5=new Progress(5,"Apr - Aug","Race Car Manufacturing & Assembly");
-const stage6=new Progress(6,"Aug - Nov","Track Testing & Vehicle Tuning");
-const stage7=new Progress(7,"Dec - Jan","Pass the Technical Inspection & Participate in Dynamic Event");
-
-const stagesInfo=[stage1,stage2,stage3,stage4,stage5,stage6,stage7];
+import { _FormulaBharat } from "../Data/OurProjects";
 
 function OurProjects()
 {
 	return(
 		<div className="page OurProjects flex-c-se-c">
-			<h1 className="page-title">Our Project - Formula Bharat</h1>
+			<h1 className="page-title">Project - Formula Bharat</h1><br />
 			<TimeLine />
 			<RenderedImage />
     	</div>
-	)
-}
-
-function RenderedImage()
-{
-	return (
-		<>
-			<h3 className="sub-title">Few Rendered Image</h3><br />
-			<span className="gray-text">Front View</span><img className="Design-Img" src={FrontLook} />	
-			<span className="gray-text">Almost Top View</span><img className="Design-Img" src={TopLook} />	
-
-		</>
 	)
 }
 
@@ -54,9 +19,20 @@ function TimeLine()
 {
 	return (
 		<div className="Timeline-Cont flex-c-se-c">
-			<h3 className="sub-title gray-text" style={{margin:'3rem'}}>Our Estimated Timeline</h3>
+			<h3 className="sub-title gray-text">Our Estimated Timeline</h3><br />
 			<Stages />
 		</div>
+	)
+}
+
+function RenderedImage()
+{
+	return (
+		<>
+			<h3 className="sub-title">Few Desgin Renders</h3><br />
+			<span className="gray-text">Front View</span><img className="Design-Img" src={FrontLook} />	
+			<span className="gray-text">Almost Top View</span><img className="Design-Img" src={TopLook} />	
+		</>
 	)
 }
 
@@ -64,20 +40,20 @@ function Stages()
 {
 	return (
 		<>
-			{stagesInfo.map((stageInfo,ind)=>{
-				return <><Stage key={ind} stageInfo={stageInfo} /><br /></>
+			{_FormulaBharat.map(({stage, duration, work})=>{
+				return <><Stage key={stage} stage={stage} duration={duration} work={work} /><br /></>
 			})}
 		</>
 	)
 }
 
-function Stage({stageInfo})
+function Stage({stage, duration, work})
 {
 	return (
 		<div className="Stage-Cont flex-c-se-c">
-			<span className="theme-title">Stage : {stageInfo.stageNum}</span>
-			<h3 className="sub-title">[ {stageInfo.timeDuration} ]</h3>
-			<span className="theme-text">"{stageInfo.mainWork}"</span>
+			<span className="theme-title">Stage : {stage}</span>
+			<h3 className="sub-title">[ {duration} ]</h3>
+			<span className="theme-text">"{work}"</span>
 		</div>
 	)
 }
