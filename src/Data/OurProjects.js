7c007c30fb717c33f83b068f477ad97f17a663c7
Roshan Thapa <roshan@Roshans-MacBook-Air-2.local>
2023-07-06 08:56:12 +0545

    Our Project data added - formula bharat

---
+++ src/Data/OurProjects.js
@@ -0,0 +1,37 @@
+export const _FormulaBharat = [
+    {
+        stage : "1",
+        duration : "Nov - Dec",
+        work : "Preliminary Research",
+    },
+    {
+        stage : "2",
+        duration : "Jan - Mar",
+        work : "Preliminary Vehicle Design, Cost Estimation & Sponsorship",
+    },
+    {
+        stage : "3",
+        duration : "Apr - May",
+        work : "Final Design Approval & Material Procurement",
+    },
+    {
+        stage : "4",
+        duration : "May - June",
+        work : "Event Registration & Quiz",
+    },
+    {
+        stage : "5",
+        duration : "Apr - Aug",
+        work : "Race Car Manufacturing & Assembly",
+    },
+    {
+        stage : "6",
+        duration : "Aug - Nov",
+        work : "Track Testing & Vehicle Tuning",
+    },
+    {
+        stage : "7",
+        duration : "Dec - Jan",
+        work : "Pass the Technical Inspection & Participate in Dynamic Event",
+    }
+]
