b5409fa2a300477af0422efd74b51930b302e773
Roshan Thapa <thaparoshan3722@gmail.com>
2023-12-04 09:35:11 +0545

    update: Partnership page

--- src/Components/Partnership.tsx
+++ src/Components/Partnership.tsx
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { _SupportInfo, _WhyUsInfo } from '../assets/data/PartnershipInfo';
+import { FaMinus, FaPlus } from "react-icons/fa";
+
+interface ISupportList
+{
+    title : string,
+    description : string,
+};
 
 const Partnership : React.FC = ()=>
 {
     return (
-        <div className="min-h-[100vh] flex flex-col justify-evenly items-center">
-            <h1 className="text-2xl font-bold">Support Us</h1>
+        <div className="min-h-[100vh] flex flex-col justify-evenly items-center pt-[8rem]">
+            <h1 className="text-4xl font-bold my-8">How you can Support us</h1>
+            <SupportUs />
+            <h1 className="text-4xl font-bold my-8">Why partner with Us</h1>
+            <WhyUs />
+        </div>
+    )
+}
+
+const SupportUs : React.FC = ()=>
+{
+    return (
+        <div className="w-[80%] flex flex-col justify-evenly items-center p-2 bg-[#333] rounded-xl">
+            {
+                _SupportInfo.map(({title, description})=>
+                {
+                    return <SupportList title={title} description={description} />
+                })
+            }
+        </div>
+    )
+}
+
+const SupportList : React.FC<ISupportList> = ({title, description} : ISupportList)=>
+{
+    const [isActive, setIsActive] = useState<boolean>(false);
+
+    return (
+        <div className="w-[100%] pt-4 px-8">
+            <div className="flex flex-row justify-between items-center">
+                <span className="text-2xl text-theme">{title}</span>
+                <a className="hover:text-theme" onClick={()=>setIsActive(!isActive)}>{!isActive?<FaPlus />:<FaMinus />}</a>
+            </div>
+            <p className="my-2">{isActive?description:""}</p>
+            <hr className="border-theme border-[0.1rem] rounded-xl my-3" />
+        </div>
+    )
+}
+
+const WhyUs : React.FC = () =>
+{
+    return (
+        <div className="flex flex-col text-left bg-[#333] p-4 w-[80%] my-8">
+            {_WhyUsInfo.map((item)=>
+            {
+                return <li className="m-2">{item}<hr className="border-theme border-[0.1rem] border-dashed rounded-xl mt-3" /></li>
+            })}
         </div>
     )
 }
---
+++ src/assets/data/PartnershipInfo.js
@@ -0,0 +1,47 @@
+export const _SupportInfo = [
+    {
+        title : "Financial Support",
+        description : "Our partners can provide critical financial support during various stages of the project to help us achieve our goals.",
+    },
+    {
+        title : "Material & Equipment Support",
+        description : "Shireto's team creates most of the components in-house, but our partners can assist us by providing the necessary materials and equipment for the fabrication of the electric car. This includes essential electrical components like batteries, motor controllers, and BMS.",
+    },
+    {
+        title : "Mentorship Support",
+        description : "Our partners can provide us with mentorship and guidance from experienced professionals in the industry, allowing us to learn and grow from their knowledge and expertise.",
+    },
+    {
+        title : "Logistics Support",
+        description : "As a Formula One team participating in international events, we require a professional logistics company to manage transportation, distribution, and travel ar rangements for our team. This includes importing parts and materials for development and ensuring smooth race car transportation to and from competi tions. A reliable logistics partner is critical to our suc cess in achieving our goals.",
+    }
+]
+
+export const _WhyUsInfo = [
+                    "Global exposure through Formula Student Events.",
+                    "Global exposure through Formula Student Events Promote motorsports culture & inspire STEM students.",
+                    "Global exposure through Formula Student Events Directly engage with talented students.",
+                    "Global exposure through Formula Student Events Access to a diverse talent pool for recruitment.",
+                    "Global exposure through Formula Student Events Collaborate on R&D projects with expertise in simulation and engineering.",
+                    "Foster relationship with educational institutions tobridge industry - academia gap.",
+                    "Platform to showcase support for International college level project while fulfilling CSR objectives."
+]
+
+export const _SponserInfo = [
+                    "GolchhaGroup",
+                    "SolidWorks",
+                    "Altair",
+                    "Neoteric",
+                    "Doro"
+]
+
+export const _FeaturedInfo = [
+                    "OnlineKhabar",
+                    "DekhaPadi",
+                    "Kantipur",
+                    "MountainTelevision",
+                    "NayaPatrika",
+                    "NepalLive",
+                    "RatoPati",
+                    "RONB"
+]
