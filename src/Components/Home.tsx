bb34ae3e78c28006472857cc4cf0b9c40474beea
Roshan Thapa <thaparoshan3722@gmail.com>
2023-12-12 20:44:01 +0545

    add: home animation and minor

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -4,12 +4,19 @@ import { _TeamInfo, _AboutUs } from "../assets/data/TeamInfo";
 const Home : React.FC = () =>
 {
 	const groupphotopath = require("../assets/images/groupphoto.png");
+	const homeVideoPath = require("../assets/video/home.mp4");
 	return (
 		<>
-		<div className="h-[100vh] w-[100vw] flex flex-col justify-evenly items-center">
-			<img className="absolute top-10rem left-0 bg-cover bg-center -z-10 brightness-50" src={groupphotopath} alt="Group photo"/>
-			<h1 className="font-extrabold text-9xl text-theme font-roboto text-transparent bg-clip-text bg-gradient-to-r from-theme-alth via-theme to-theme-alth bg-300% animate-gradient drop-shadow-xl">{_TeamInfo.name}</h1>
-			<p className="mx-[4rem] p-4 bg-theme-alth text-justify rounded-md text-xl">{_TeamInfo.description}</p>
+		<div className="min-h-[100vh] pt-[100vh] w-[100vw] flex flex-col justify-evenly items-center">
+			<video src={homeVideoPath} loop={true} autoPlay={true} className="absolute top-[10vh] left-0 w-[100vw] brightness-75 -z-10 h-[90vh]" />
+			
+			<div id="bottom" className="min-h-[120vh] w-[100vw] flex flex-col justify-evenly items-center">
+				<h1 className="font-extrabold text-9xl text-theme font-roboto text-transparent bg-clip-text bg-gradient-to-r from-theme-alth via-theme to-theme-alth bg-300% animate-gradient drop-shadow-xl">{_TeamInfo.name}</h1> 
+				<div className="flex flex-row justify-between w-[100%] items-center p-12">
+					<img className="bg-cover bg-center w-[60vw] rounded-xl -z-10 brightness-75" src={groupphotopath} alt="Group photo"/>
+					<p className="p-12 bg-theme-alt text-justify rounded-md text-xl">{_TeamInfo.description}</p>
+				</div>
+			</div>
 		</div>
 		</>
 	)
